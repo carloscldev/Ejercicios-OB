d1f09eb9f5dcb26bf779db882cf031d80a44c338
carloscldev <96172647+carloscldev@users.noreply.github.com>
2022-03-22 15:32:55 +0000

    Create ejercicio2.js

---
+++ 4.Cadenas de texto/ejercicio2.js
@@ -0,0 +1,4 @@
+/* Como segundo ejercicio, tienes que crear otra variable con una cadena de texto y convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola. */
+
+let quijote ="En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.";
+console.log(quijote.toUpperCase());
