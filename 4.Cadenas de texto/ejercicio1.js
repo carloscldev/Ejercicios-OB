0d1bb6348650bd6b602149f2f0f7f71bda2c3ea9
carloscldev <96172647+carloscldev@users.noreply.github.com>
2022-03-22 15:29:56 +0000

    Create ejercicio1.js

---
+++ 4.Cadenas de texto/ejercicio1.js
@@ -0,0 +1,6 @@
+/* En este primer ejercicio tienes que crear una cadena de texto con una frase, por ejemplo: “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. Por consola tienes que mostrar la frase con las palabras cambiadas. */
+
+let vecina = "El perro de mi vecina es muy molesto";
+let vecino = vecina.replace("vecina","vecino");
+
+console.log (vecino);
