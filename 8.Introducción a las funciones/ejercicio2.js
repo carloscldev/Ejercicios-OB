92ea76db7f46f9e143a38a73946f12179caed4e1
carloscldev <96172647+carloscldev@users.noreply.github.com>
2022-03-24 16:56:10 +0000

    Create ejercicio2.js

---
+++ 8.Introducción a las funciones/ejercicio2.js
@@ -0,0 +1,10 @@
+/* En este ejercicio tienes que crear un archivo javascript que contenga una función llamada eliminarUltimo() que reciba como parámetro un array y elimine el último elemento de este. */
+
+let verduras = ['acelga', 'coliflor', 'judias', 'perejil', 'zanahoria'];
+function eliminarUltimo(){
+  document.write(verduras.pop()); 
+}
+
+console.log(verduras)
+eliminarUltimo()
+console.log(verduras)
